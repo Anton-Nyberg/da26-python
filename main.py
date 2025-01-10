b1d31ea6bd92b1f889c716d05f7b280df9387856
Carlbergqvist <carl.bergqvist@hyperisland.se>
2025-01-10 14:55:06 +0100

    Code for filtering on lounge and peaceful tagged songs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print('Hello world')
-import pandas as pd
-print("testing testing")
-print("hej hej")
+final_data["Peaceful_lounge_music"] = (
+    (final_data["danceability"].between(0.2, 0.6)) &
+    (final_data["tempo"] < 110) &
+    (final_data["energy"].between(0.1, 0.4)) &
+    (final_data["speechiness"] < 0.3)
+).astype(int)
+lounge_and_peaceful_music = final_data[final_data["Peaceful_lounge_music"] == 1]
+lounge_and_peaceful_music_unique = lounge_and_peaceful_music.drop_duplicates(subset="name_y", keep='first')
