c1847da8214c8b9ef9c73e064c526581897bfe00
Carlbergqvist <carl.bergqvist@hyperisland.se>
2025-01-13 11:50:03 +0100

    part of the function for creating the buttons

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-final_data["Peaceful_lounge_music"] = (
-    (final_data["danceability"].between(0.2, 0.6)) &
-    (final_data["tempo"] < 110) &
-    (final_data["energy"].between(0.1, 0.4)) &
-    (final_data["speechiness"] < 0.3)
-).astype(int)
-lounge_and_peaceful_music = final_data[final_data["Peaceful_lounge_music"] == 1]
-lounge_and_peaceful_music_unique = lounge_and_peaceful_music.drop_duplicates(subset="name_y", keep='first')
+def filter_music(data, mode):
+    # Creating the conditions for each playlist
+    if mode == "Peaceful_lounge":
+        condition = (
+            (data["danceability"].between(0.2, 0.6)) &
+            (data["tempo"] < 110) &
+            (data["energy"].between(0.1, 0.4)) &
+            (data["speechiness"] < 0.3)
+        )
+    elif mode == "acoustic":
+        condition = (
+            
+        )
+    elif mode == "relaxing":
+        condition = (
+            
+        )
+    elif mode == "dancing":
+        condition = (
+            
+        )
+    elif mode == "energetic_workout":
+        condition = (
+            
+        )
+    filtered_data = data[condition]
+    return filtered_data.drop_duplicates(subset="title", keep="first")
